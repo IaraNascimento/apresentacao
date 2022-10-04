f52a7148be667aabb9ad4c90218ef3485fff0ea4
Iara Nascimento Sztybe <iara.sztybe@gmail.com>
2022-10-04 00:30:11 -0300

    Finalizando mapeamento de links e videos de 'I As Famílias Linguísticas', 'II A Escrita Das Línguas Indígenas' e 'Regiões Multilíngues no Brasil', adicionando descanso de tela e adicionando palavras ilustradas (B, C e D)

---
+++ js/idle.js
@@ -0,0 +1,46 @@
+
+addVideoOnBody();
+const bodyContent = document.getElementById("content");
+const movie = document.getElementById("movie");
+const homeLocation = localStorage.getItem('homeLocation');
+const maxIdleTime = 5*60;
+movie.style.display = 'none';
+localStorage.setItem('idleTime', 0);
+
+function playIdleVideo() {
+    bodyContent.style.display = 'none';
+    movie.style.display = 'block';
+    movie.currentTime = 0;
+    movie.onclick = () => {
+        document.location = homeLocation;
+    };
+}
+
+function idleTimeLooping() {
+    setTimeout(() => {
+        const videoTime = JSON.parse(localStorage.getItem('videoTime'));
+        const idleTime = JSON.parse(localStorage.getItem('idleTime')) + 5;
+        localStorage.setItem('idleTime', idleTime);
+        console.log(idleTime);
+        if (idleTime >= (maxIdleTime + videoTime)) {
+            playIdleVideo();
+        } else {
+            idleTimeLooping();
+        }
+    }, 5000);
+}
+
+function addVideoOnBody() {
+    const span = document.createElement('span');
+    span.innerHTML = ''
+        + '<video id="movie" preload="auto" autoplay loop muted>'
+        + '<source src="' + localStorage.getItem('videoLocation') + '" type="video/mp4" />'
+        + '</video>';
+    document.getElementsByTagName("body")[0].prepend(span);
+}
+
+bodyContent.onclick = () => {
+    localStorage.setItem('idleTime', 0);
+}
+
+idleTimeLooping();
--- js/video.js
+++ js/video.js
@@ -2,19 +2,26 @@ function executeVideo(video, buildDefault) {
 	document.getElementById("content").style.backgroundImage = "";
 	document.getElementById("content").innerHTML = ''
 		+ '<span id="closeVideo">fechar X</span>'
-		+ '<video id="movie">'
+		+ '<video id="current-movie">'
 		+ '<source src="'+video+'" type="video/mp4" />'
 		+ '</video>';
-	const movie = document.getElementById("movie");
-	movie.addEventListener('ended', handlerEnd, false);
+	const currentMovie = document.getElementById("current-movie");
+	currentMovie.addEventListener('loadedmetadata', () => {
+		localStorage.setItem('videoTime', currentMovie.duration);
+	});
+	currentMovie.addEventListener('ended', handlerEnd, false);
 	const closeVideo = document.getElementById("closeVideo");
 	closeVideo.onclick = () => {
-		movie.pause();
+		currentMovie.pause();
+		localStorage.setItem('idleTime', 0);
+		localStorage.removeItem('videoTime');
 		buildDefault();
 	};
-	movie.play();
+	currentMovie.play();
     function handlerEnd(e) {
 		setTimeout(() => {
+			localStorage.setItem('idleTime', 0);
+			localStorage.removeItem('videoTime');
 			buildDefault();
 		}, 1000);
     }
